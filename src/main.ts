800e1a4d96c51493885c249c124a09d52e81f2ac
Jason Xu <Xinwen31415@gmail.com>
2023-10-09 17:03:37 -0500

    added redirect filter to default list of global filters

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import * as cookieParser from 'cookie-parser';
 import { config } from 'dotenv';
 import { AppModule } from './app.module';
 import { ErrorPageFilter } from './utils/filters/error-page.filter';
+import { RedirectFilter } from './utils/filters/redirect.filter';
 
 config();
 
@@ -12,7 +13,7 @@ async function bootstrap() {
 
 	app.use(cookieParser())
 		.useGlobalPipes(new ValidationPipe({ transform: true, transformOptions: { enableImplicitConversion: true } }))
-		.useGlobalFilters(new ErrorPageFilter(app.get(HttpAdapterHost).httpAdapter));
+		.useGlobalFilters(new ErrorPageFilter(app.get(HttpAdapterHost).httpAdapter), new RedirectFilter());
 
 	await app.listen(process.env.PORT || 5000);
 }
